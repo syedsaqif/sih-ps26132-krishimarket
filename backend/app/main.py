b7407c94f99726fd81c3735e595947c863ec14e1
syedsaqif <syedsaqif03@gmail.com>
2026-09-12 00:45:03 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -91,6 +91,7 @@ else:
         "http://localhost:3001",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
+        "https://krishimarket-frontend.onrender.com",
     ]
 
 app.add_middleware(
